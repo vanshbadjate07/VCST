b54a2f805216ed07e4748960de943e5213c39a00
Vaishnavi Nikumbh <vaishnavi.official30@gmail.com>
2025-02-28 12:17:00 +0530

    Java COde DIvision Code done

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 class Hello{
     public static void main(String args[]){
-        System.out.println("Hello world");
+        
+        int a = 5;
+        int b = 2;
+
+        System.out.println("Division is: " + (a/b));
+        System.out.println("Thank you for running my program");
+
     }
 }
